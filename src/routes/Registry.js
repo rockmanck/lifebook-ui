96c01735e040e53e7d6b0e8e3991a236a6882102
rockmanck <rockman.roll@gmail.com>
2022-06-04 13:36:15 +0300

    Added useState and useEffect hooks example

--- src/routes/Registry.js
+++ src/routes/Registry.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
 
 function Registry() {
+    const [registryData, setRegistryData] = useState([])
+    const [textInput, setTextInput] = useState("")
+    const [error, setError] = useState(false);
+
+    const addItem = (e) => {
+        e.preventDefault();
+
+        const tempData = [...registryData]; // prepend previous data - spread operator
+        tempData.push(textInput);
+        setRegistryData(tempData);
+        setTextInput("");
+    }
+
+    console.log(registryData);
+
+    // subscribe on each change to input text
+    // if length > 13 chars, fire error
+    useEffect(() => {
+        if (textInput.length > 13) setError(true);
+        else                       setError(false);
+    }, [textInput]);
+
+
     return (
         <div>
             <h1>Registry page</h1>
             <Link to="/">Go to Home page</Link>
+
+            <form onSubmit={addItem}>
+                <label>
+                    text input:
+                    <input type="text" value={textInput}
+                           onChange={(e) => setTextInput(e.target.value)} />
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+            {error ? <span style={{color: "red"}}>Error occured.</span> : null}
         </div>
     )
 }
